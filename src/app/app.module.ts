590126090316016c7e9046f5f9e9a2c15eb8cebe
cplan85 <carlos_planchart@live.com>
2022-11-12 22:12:23 +0100

    home component setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeMainComponent } from './home/home-main/home-main.component';
+import { HeroComponent } from './home/components/hero/hero.component';
+import { AboutComponent } from './home/components/about/about.component';
+import { ServicesComponent } from './home/components/services/services.component';
+import { PortfolioComponent } from './home/components/portfolio/portfolio.component';
+import { BlogPostsComponent } from './home/components/blog-posts/blog-posts.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeMainComponent,
+    HeroComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    BlogPostsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
